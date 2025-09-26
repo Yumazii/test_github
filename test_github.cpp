5e889107a5fea7c4667da724ea337381581ba5f1
yumazii <siyuan.jiang@polymtl.ca>
2025-09-25 23:18:37 -0400

    first commit

---
+++ test_github.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Includes the iostream library for input/output operations
+
+int main() { // The main function, the entry point of the program
+    std::cout << "Hello World!" << std::endl; // Prints "Hello World!" to the console and moves to a new line
+    return 0; // Indicates successful program execution
+}
